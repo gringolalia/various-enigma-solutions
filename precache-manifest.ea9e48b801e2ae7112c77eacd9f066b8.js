5b3c9f3f6173d24a64ab42f8d1d489f4439f3db2
Colin Brayton <gringolalia@gmail.com>
2020-05-11 19:24:17 -0300

    deploy

---
+++ precache-manifest.ea9e48b801e2ae7112c77eacd9f066b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d2439a7e81cc6244b2",
+    "url": "/js/about.1eb1e973.js"
+  },
+  {
+    "revision": "5d85a0385a225d61fd86",
+    "url": "/css/app.c9ea83de.css"
+  },
+  {
+    "revision": "5d85a0385a225d61fd86",
+    "url": "/js/app.8dd62da4.js"
+  },
+  {
+    "revision": "5511433b5979f385616e",
+    "url": "/css/chunk-vendors.fcb0750d.css"
+  },
+  {
+    "revision": "5511433b5979f385616e",
+    "url": "/js/chunk-vendors.359ab158.js"
+  },
+  {
+    "revision": "3fea563c5316b0039389b59f0a876105",
+    "url": "/img/hammer-sickle.3fea563c.svg"
+  },
+  {
+    "revision": "abc24807c0725219a6c6336d4483df9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d0f1abd34be8edd62353a8cd9625480",
+    "url": "/bespoke.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
